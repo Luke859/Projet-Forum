75624ce6b13a8324dada5a9fedfd0715c4689cfd
PCOME <pierric.come@ynov.com>
2021-06-03 16:00:11 +0200

    add user db

--- src/BDD/bdd.go
+++ src/BDD/bdd.go
@@ -1,22 +1,39 @@
+package main
+
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func gestionData() {
+func gestionData() int {
 	db, err := sql.Open("sqlitte3", "./nomBase.db")
 	if err != nil {
 		fmt.Print("error ouvertur base")
-		return (5)
+		return (500)
+	}
+	pseudo := ""
+	Hmpd := ""
+	statusNewUser := newUser(pseudo, Hmpd, db)
+	if statusNewUser == 0 {
+		return (0)
+	} else {
+		fmt.Print("probleme --> creation user")
+		return (500)
 	}
 }
 
-func newUser(pseudo, Hmpd) {
+func newUser(pseudo string, Hmpd string, db *sql.DB) int {
 	statement, err := db.Prepare("INSERT INTO User (pseudo, Hmpd) VALUES(?,?)")
 	if err != nil {
 		fmt.Print("error new user")
 		return (5)
 	}
-	statement.Exec()
+	statement.Exec(pseudo, Hmpd)
+	return (0)
+}
+
+func checkUser(pseudo string, Hmpd string, db *sql.DB) int {
+	return (0)
 }
