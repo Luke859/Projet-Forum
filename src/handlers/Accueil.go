a47392c616917c46380def0615b9a833487fd657
nathymellal <72868466+nathymellal@users.noreply.github.com>
2021-06-03 09:29:37 +0200

    Update Accueil.go

--- src/handlers/Accueil.go
+++ src/handlers/Accueil.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/handlers/Accueil.go
 package Home
 
 import (
@@ -18,3 +19,25 @@ func AccueilPage(w http.ResponseWriter, r *http.Request) {
 	}
 	t.Execute(w, nil)
 }
+=======
+package Home
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var t *template.Template
+var tErr *template.Template
+
+func AccueilPage(w http.ResponseWriter, r *http.Request) {
+	// Déclaration des fichiers à parser
+	t, err := template.ParseFiles("HTML/layout.html", "HTML/accueil.html", "HTML/navbar.html")
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+		return
+	}
+	t.Execute(w, nil)
+}
+>>>>>>> parent of deb4d99 (Update accueil.go):src/Accueil/accueil.go
