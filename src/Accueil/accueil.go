deb4d997e932c788a5767f0afe0db892fbe46495
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-06-03 09:00:33 +0200

    Update accueil.go

--- src/Accueil/accueil.go
+++ src/Accueil/accueil.go
@@ -11,7 +11,7 @@ var tErr *template.Template
 
 func AccueilPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
-	t, err := template.ParseFiles("HTML/layout.html", "HTML/accueil.html", "HTML/navbar.html")
+	t, err := template.ParseFiles("Static/HTML/layout.html", "Static/HTML/accueil.html", "Static/HTML/navbar.html")
 	if err != nil {
 		log.Fatalf("Template execution: %s", err)
 		return
