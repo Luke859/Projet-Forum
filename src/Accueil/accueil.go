872305f8ec499ce11a193adb2c3aa099d19fbb09
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-06-03 10:55:16 +0200

    delete

--- src/Accueil/accueil.go
+++ src/Accueil/accueil.go
@@ -6,9 +6,6 @@ import (
 	"text/template"
 )
 
-var t *template.Template
-var tErr *template.Template
-
 func AccueilPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
 	t, err := template.ParseFiles("static/HTML/layout.html", "static/HTML/accueil.html", "static/HTML/navbar.html")
