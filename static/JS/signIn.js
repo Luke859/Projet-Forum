6f7eb6dd469f153a26f2bb7e70b1e0138616e629
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2021-06-03 11:41:03 +0200

    restoration

--- static/JS/signIn.js
+++ static/JS/signIn.js
@@ -3,10 +3,5 @@ console.log("Script signIn loaded")
 let pseudo = document.getElementById('pseudo').value
 let password = document.getElementById('password').value
 
-const userRoutes = require('/routes/user');
-
-signIn.use('/api/stuff', stuffRoutes);
-signIn.use('/api/auth', userRoutes);
-
 console.log(pseudo);
 console.log(password);
