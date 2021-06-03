4a44455eb178831f9e150797a39c5dd49c9fd6b5
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2021-06-03 09:40:03 +0200

    test

--- Static/JS/signIn.js
+++
@@ -1,7 +0,0 @@
-console.log("Script signIn loaded")
-
-let pseudo = document.getElementById('pseudo').value
-let password = document.getElementById('password').value
-
-console.log(pseudo);
-console.log(password);
