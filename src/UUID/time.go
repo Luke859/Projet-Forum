a546c80bc00ce55c7804d76b15dc1ad6621967fc
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2021-06-10 09:59:15 +0200

    Update time.go

--- src/UUID/time.go
+++ src/UUID/time.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-// A Time represents a time as the number of 100's of nanoseconds since 15 Oct
-// 1582.
 type Time int64
 
 const (
@@ -26,8 +24,6 @@ var (
 	timeNow = time.Now // for testing
 )
 
-// UnixTime converts t the number of seconds and nanoseconds using the Unix
-// epoch of 1 Jan 1970.
 func (t Time) UnixTime() (sec, nsec int64) {
 	sec = int64(t - g1582ns100)
 	nsec = (sec % 10000000) * 100
@@ -35,9 +31,6 @@ func (t Time) UnixTime() (sec, nsec int64) {
 	return sec, nsec
 }
 
-// GetTime returns the current Time (100s of nanoseconds since 15 Oct 1582) and
-// clock sequence as well as adjusting the clock sequence as needed.  An error
-// is returned if the current time cannot be determined.
 func GetTime() (Time, uint16, error) {
 	defer timeMu.Unlock()
 	timeMu.Lock()
@@ -47,14 +40,11 @@ func GetTime() (Time, uint16, error) {
 func getTime() (Time, uint16, error) {
 	t := timeNow()
 
-	// If we don't have a clock sequence already, set one.
 	if clockSeq == 0 {
 		setClockSequence(-1)
 	}
 	now := uint64(t.UnixNano()/100) + g1582ns100
 
-	// If time has gone backwards with this clock sequence then we
-	// increment the clock sequence
 	if now <= lasttime {
 		clockSeq = ((clockSeq + 1) & 0x3fff) | 0x8000
 	}
@@ -62,13 +52,6 @@ func getTime() (Time, uint16, error) {
 	return Time(now), clockSeq, nil
 }
 
-// ClockSequence returns the current clock sequence, generating one if not
-// already set.  The clock sequence is only used for Version 1 UUIDs.
-//
-// The uuid package does not use global static storage for the clock sequence or
-// the last time a UUID was generated.  Unless SetClockSequence is used, a new
-// random clock sequence is generated the first time a clock sequence is
-// requested by ClockSequence, GetTime, or NewUUID.  (section 4.2.1.1)
 func ClockSequence() int {
 	defer timeMu.Unlock()
 	timeMu.Lock()
@@ -82,8 +65,6 @@ func clockSequence() int {
 	return int(clockSeq & 0x3fff)
 }
 
-// SetClockSequence sets the clock sequence to the lower 14 bits of seq.
-// Setting to -1 causes a new sequence to be generated.
 func SetClockSequence(seq int) {
 	defer timeMu.Unlock()
 	timeMu.Lock()
@@ -103,8 +84,6 @@ func setClockSequence(seq int) {
 	}
 }
 
-// Time returns the time in 100s of nanoseconds since 15 Oct 1582 encoded in uuid.
-//The time is only defined for version 1 and 2 UUIDs.
 func (uuid UUID) Time() Time {
 	time := int64(binary.BigEndian.Uint32(uuid[0:4]))
 	time |= int64(binary.BigEndian.Uint16(uuid[4:6])) << 32
@@ -112,8 +91,6 @@ func (uuid UUID) Time() Time {
 	return Time(time)
 }
 
-// ClockSequence returns the clock sequence encoded in uuid.
-// The clock sequence is only well defined for version 1 and 2 UUIDs.
 func (uuid UUID) ClockSequence() int {
 	return int(binary.BigEndian.Uint16(uuid[8:10])) & 0x3fff
 }
