1a493a75d792c50437ad6cd6d39427d3555a71a0
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2021-06-14 13:45:05 +0200

    test cookie

---
+++ src/Cookie/cookie3.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/brafales/go-session/handlers"
+)
+
+func main() {
+	cookieName := os.Getenv("COOKIE_NAME")
+	cookieValue := os.Getenv("COOKIE_VALUE")
+	cookieDomain := os.Getenv("COOKIE_DOMAIN")
+	cookiePath := os.Getenv("COOKIE_PATH")
+	cookieDuration := os.Getenv("COOKIE_DURATION")
+
+	cookieDurationInteger, err := strconv.Atoi(cookieDuration)
+
+	if err != nil {
+		panic(err)
+	}
+
+	handler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("OK"))
+	})
+
+	loginHandler := handlers.Login{Name: cookieName,
+		Domain: cookieDomain,
+		Path:   cookiePath,
+		Value:  cookieValue,
+		MaxAge: cookieDurationInteger,
+		Next:   handler,
+	}
+
+	logoutHandler := handlers.Logout{
+		Name:   cookieName,
+		Domain: cookieDomain,
+		Path:   cookiePath,
+		Next:   handler,
+	}
+
+	http.Handle("/login", loginHandler)
+	http.Handle("/logout", logoutHandler)
+
+	address := fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	if err := http.ListenAndServe(address, nil); err != nil {
+		panic(err)
+	}
+}
