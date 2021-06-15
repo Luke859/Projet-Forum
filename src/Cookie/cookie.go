ff952061076fc7381536113138f946ca526b91f1
FDAGNAS <florian.dagnas@ynov.com>
2021-06-15 16:44:49 +0200

    debug

--- src/Cookie/cookie.go
+++ src/Cookie/cookie.go
@@ -1,18 +1,27 @@
-document.cookie = "test1=Hello; SameSite=None; Secure; HttpOnly";
-document.cookie = "test2=World; SameSite=None; Secure; HttpOnly";
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+)
+
+func main() {
+	myuuid, err := uuid.NewV4()
+	if err == nil{
+		fmt.Println("Your UUID is:", myuuid)
+	}
+}
+
+
+document.cookie = "test1="+ myuuid + "; SameSite=None; Secure; HttpOnly";
 
 const cookieValue1 = document.cookie
 	.split('; ')
 	.find(row => row.startsWith('test1='))
 	.split('=')[1];
 
-const cookieValue2 = document.cookie
-	.split('; ')
-	.find(row => row.startsWith('test2='))
-	.split('=')[1];
-
-function showCookieValue() {
+func showCookieValue() {
 	const output = document.getElementById('cookie-value')
-	output.textContent = '> ' + cookieValue1 + " " + cookieValue2
+	output.textContent = '> ' + cookieValue1
 	return output
 }
