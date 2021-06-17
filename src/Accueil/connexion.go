0e24fbe0a609d8740fe1921c7eb51566fdfc74b1
FDAGNAS <florian.dagnas@ynov.com>
2021-06-17 15:49:24 +0200

    cookie

--- src/Accueil/connexion.go
+++ src/Accueil/connexion.go
@@ -4,7 +4,16 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+	"io"
+    "fmt"
+    "time"
 )
+
+func WriteCookieServer(w http.ResponseWriter, req *http.Request) {
+    expire := time.Now().AddDate(0, 0, 1)
+    cookie := http.Cookie{Name: "testcookiename", Value: "testcookievalue", Path: "/", Expires: expire, MaxAge: 86400}
+    http.SetCookie(w, &cookie)
+}
 
 func ConnexionPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
@@ -14,5 +23,6 @@ func ConnexionPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	t.Execute(w, nil)
+	WriteCookieServer(w)
 
 }
