42f54658ecba54ce58a17a3191e70d79db20d9fa
luke.jones <lukebryan@orange.fr>
2021-06-18 11:39:43 +0200

    cookie

--- src/Accueil/connexion.go
+++ src/Accueil/connexion.go
@@ -8,27 +8,22 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-func WriteCookieServer(w http.ResponseWriter, req *http.Request) {
-    myuuid, err := uuid.NewV4()
-    if err != nil {
-        http.SetCookie(w,&http.Cookie{
-            Name:       "CookieUUID",
-            Value:      myuuid.String(),
-            Domain:     "ProjetForum",
-            Secure:     true,
-            HttpOnly:   true,
-        })
-    }
-}
-
 func ConnexionPage(w http.ResponseWriter, r *http.Request) {
 	// Déclaration des fichiers à parser
 	t, err := template.ParseFiles("static/HTML/layout.html", "static/HTML/connexion.html", "static/HTML/navbar.html")
 	if err != nil {
 		log.Fatalf("Template execution: %s", err)
-		myuuid, _ := uuid.NewV4()
+		/*myuuid, _ := uuid.NewV4()
 		expire := time.Now().AddDate(0, 0, 1)
-		cookie := http.Cookie{Name: "testcookiename", Value: myuuid.String(), Path: "/", Expires: expire, MaxAge: 86400}
+		cookie := http.Cookie{
+			Name: "testcookiename",
+			Value: myuuid.String(),
+			Path: "/",
+			Expires: expire,
+			MaxAge: 86400,
+			Secure:     true,
+            HttpOnly:   true,
+		}*/
 
     	http.SetCookie(w, &cookie)
 		t.Execute(w, nil)
