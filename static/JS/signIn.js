bc4601c5f7124a7e141961b071517cd40f763805
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2021-06-22 14:43:13 +0200

    update florian

--- static/JS/signIn.js
+++ static/JS/signIn.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-let form = document.getElementById('form')
-
-function formRecup() {
-    form.submit()
-}
-
-let formconnect = document.getElementById('formconnect')
-
-function formRecupConnect(){
-    formconnect.submit()
-}
-=======
-/* console.log("Script signIn loaded")
-
-let pseudo = document.getElementById('pseudo').value
-let password = document.getElementById('password').value
-
-console.log(pseudo);
-console.log(password);
-
-
-*/
 
 let form = document.getElementById('form')
 
@@ -39,4 +16,3 @@ let textarea = document.getElementById('textarea')
     function TextRecup(){
         textarea.submit()
     }
->>>>>>> Dev
