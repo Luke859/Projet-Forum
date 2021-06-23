6ac95a7bc7e69ba9a99233354c91ac35946f643b
luke <luke.jones@ynov.com>
2021-06-23 10:00:50 +0200

    ajout

--- Static/JS/like.js
+++ Static/JS/like.js
@@ -1,20 +1,23 @@
-var like = document.getElementById('addlike');
+// function changeLike() {
+//     like.textContent = "1";
 
+//     if(like.textContent === "1") {
+//         document.onclick = change2Like;
+//     }
+
+// }
+
+// function change2Like() {
+//     like.textContent ="0";
+
+//     if(like.textContent === "0") {
+//         document.onclick = changeLike;
+//     }
+// }
 document.onclick = changeLike;
 
-function changeLike() {
-    like.textContent = "1";
+function changeLike(){
 
-    if(like.textContent === "1") {
-        document.onclick = change2Like;
-    }
-
-}
-
-function change2Like() {
-    like.textContent ="0";
-
-    if(like.textContent === "0") {
-        document.onclick = changeLike;
-    }
+    var like = document.getElementById('like');
+    like.submit()
 }
