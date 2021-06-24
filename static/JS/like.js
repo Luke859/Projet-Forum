8b7bc65646eb698a62acc055875b7247e8838899
luke.jones <lukebryan@orange.fr>
2021-06-24 09:54:36 +0200

    update

--- static/JS/like.js
+++ static/JS/like.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:static/JS/like.js
 var like = document.getElementById('addlike');
 
 document.onclick = changeLike;
@@ -19,30 +18,4 @@ function change2Like(e) {
         document.onclick = changeLike;
     }
 }
-=======
-document.onclick = changeLike;
 
-function changeLike() {
-    like.textContent = "1";
-
-    if(like.textContent === "1") {
-        document.onclick = change2Like;
-    }
-
-}
-
-function change2Like() {
-    like.textContent ="0";
-
-    if(like.textContent === "0") {
-        document.onclick = changeLike;
-    }
-}
-
-
-// function changeLike(){
-
-//     var like = document.getElementById('like');
-//     like.submit()
-// }
->>>>>>> 85fb1ee1353def558eb2ee15fc16152ac7cb0006:Static/JS/like.js
