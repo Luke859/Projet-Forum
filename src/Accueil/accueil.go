347c5fefc1263455f8a9594dea089ec0e3a21a8a
PCOME <pierric.come@ynov.com>
2021-06-29 09:38:56 +0200

    clean up

--- src/Accueil/accueil.go
+++ src/Accueil/accueil.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 
 	BDD "../BDD"
 )
 
 type PageAccueil struct {
-	Post string
-	Cmt  string
+	Id_Post int
+	Post    string
+	Cmt     []string
+	Like    int
 }
 
 func AccueilPage(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +25,26 @@ func AccueilPage(w http.ResponseWriter, r *http.Request) {
 	_, db := BDD.GestionData()
 
 	_, postsDouble = BDD.GetAllPost(db)
+	//_, postsDouble = BDD.IsLikedPOST(db, 1)
 
 	for _, postSync := range postsDouble {
+
+		id_post, _ := strconv.Atoi(postSync[0])
+		_, cmtsDouble := BDD.GetAllCmt(db, id_post)
+
 		p := PageAccueil{
-			Post: postSync[1],
+			Post:    postSync[1],
+			Cmt:     make([]string, 0),
+			Id_Post: id_post,
+			// Like: postSync[1],
 		}
+		fmt.Println(p.Id_Post)
+		for _, cmtSync := range cmtsDouble {
+			p.Cmt = append(p.Cmt, cmtSync[2])
+		}
+
 		postOne = append(postOne, p)
+
 	}
 
 	t, err := template.ParseFiles("static/HTML/layout.html", "static/HTML/Accueil.html", "static/HTML/navbar.html")
