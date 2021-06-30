a1072b8f3c2cb3eba2cce504c9be17c67bf91158
PCOME <pierric.come@ynov.com>
2021-06-30 09:48:13 +0200

    les likes

--- static/JS/like.js
+++ static/JS/like.js
@@ -1,23 +1,21 @@
-let like = document.getElementById('addlike')
 
-function changeLike() {
 
+function changeLike(id) {
+
+    let like = document.getElementById(id)
     like.textContent = "Liked post ❤️" + 1
 
     if(like.textContent === "Liked post ❤️" + 1){
-        like.onclick() = deleteLike;
+        like.onclick = deleteLike;
     }
-    
-    like.submit()
 }
 
-function deleteLike(){
-
+function deleteLike(id){
+    
+    let like = document.getElementById(id)
     like.textContent = "Disliked post 💔" + 0
 
     if(like.textContent === "Disliked post 💔" + 0){
-        like.onclick() = changelike();
+        like.onclick = changeLike;
     }
-    
-    like.submit()
 }
